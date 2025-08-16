890798b5c7365e90ce4c62b792475dccd4e2b0d0
Ibnul Mohammad Adib <adib.bdhk@gmail.com>
2025-08-16 23:18:48 +0600

    flutter backend

---
+++ frontend/lib/services/api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class Api{
+    static const String baseUrl = "http://127.0.01:5000";
+
+    static Future<Map<String, dynamic>> login(String email, String password) async{
+        final response = await http.post(
+            Uri.parse("$baseUrl/login"),
+            headers: {"Content-Type":"application/json"},
+            body: jsonEncode({"email":email, "password":password}),
+            );
+        return jsonDecode(response.body);
+    }
+
+    
+    static Future<Map<String, dynamic>> register(String email, String password) async{
+        final response = await http.post(
+            Uri.parse("$baseUrl/login"),
+            headers: {"Content-Type":"application/json"},
+            body: jsonEncode({"email":email, "password":password}),
+            );
+        return jsonDecode(response.body);
+    }
+
+}
